a5bebbc4198075366c1fad5b558421915d73f9b4
Akash <akashrajraut318@gmail.com>
2023-04-25 14:03:45 +0530

    Added Post controller and service

--- src/main/java/com/Flat101Blog/blog/BlogController/PostContoller.java
+++ src/main/java/com/Flat101Blog/blog/BlogController/PostContoller.java
@@ -1,4 +1,30 @@
 package com.Flat101Blog.blog.BlogController;
 
+import com.Flat101Blog.blog.BlogEntity.Post;
+import com.Flat101Blog.blog.BlogService.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PostContoller {
+
+    PostService postservice;
+
+    @Autowired
+    public PostContoller(PostService postservice) {
+        this.postservice = postservice;
+    }
+
+
+    @PostMapping
+     public void AddPost(@RequestBody Post post)
+     {
+         postservice.AddPost(post);
+     }
+
+
+
+
 }
--- src/main/java/com/Flat101Blog/blog/BlogRepository/PostRepository.java
+++ src/main/java/com/Flat101Blog/blog/BlogRepository/PostRepository.java
@@ -1,4 +1,9 @@
 package com.Flat101Blog.blog.BlogRepository;
 
-public interface PostRepository {
+import com.Flat101Blog.blog.BlogEntity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository  extends JpaRepository<User,Integer>{
 }
